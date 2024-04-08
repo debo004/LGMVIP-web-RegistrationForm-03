b5019dc4bca7b2a08940a22aa0d6da924957ed0f
Debopriyo Goswami <126260211+debo004@users.noreply.github.com>
2024-04-09 00:19:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("registrationForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  var fullName = document.getElementById("fullName").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var imageLink = document.getElementById("imageLink").value;
+  var interests = [];
+  var checkboxes = document.querySelectorAll('input[name="interests"]:checked');
+  checkboxes.forEach(function (checkbox) {
+    interests.push(checkbox.value);
+  });
+  var gender = document.getElementById("gender").value;
+
+  var resultDiv = document.getElementById("result");
+  resultDiv.innerHTML = "<h3>Registration Successful!</h3>";
+  resultDiv.innerHTML += "<p><strong>Full Name:</strong> " + fullName + "</p>";
+  resultDiv.innerHTML += "<p><strong>Email:</strong> " + email + "</p>";
+  resultDiv.innerHTML += "<p><strong>Password:</strong> " + password + "</p>";
+  resultDiv.innerHTML += "<p><img src='" + imageLink + "' alt='User Image' style='width:200px ; height: 200px'></p>";
+  resultDiv.innerHTML += "<p><strong>Interests:</strong> " + interests.join(', ') + "</p>";
+  resultDiv.innerHTML += "<p><strong>Gender:</strong> " + gender + "</p>";
+});
