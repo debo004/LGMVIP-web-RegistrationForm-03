1050ac37623d35c58b9a0defe461b458e6403194
Debopriyo Goswami <126260211+debo004@users.noreply.github.com>
2024-04-12 16:20:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ document.getElementById("registrationForm").addEventListener("submit", function 
   var gender = document.getElementById("gender").value;
 
   var resultDiv = document.getElementById("result");
-  resultDiv.innerHTML = "<h3>Registration Successful!</h3>";
+  resultDiv.innerHTML = "<h3 style='color: green'>Registration Successful!</h3>";
   resultDiv.innerHTML += "<p><strong>Full Name:</strong> " + fullName + "</p>";
   resultDiv.innerHTML += "<p><strong>Email:</strong> " + email + "</p>";
   resultDiv.innerHTML += "<p><strong>Password:</strong> " + password + "</p>";
-  resultDiv.innerHTML += "<p><img src='" + imageLink + "' alt='User Image' style='width:200px ; height: 200px'></p>";
   resultDiv.innerHTML += "<p><strong>Interests:</strong> " + interests.join(', ') + "</p>";
   resultDiv.innerHTML += "<p><strong>Gender:</strong> " + gender + "</p>";
+  resultDiv.innerHTML += "<p><img src='" + imageLink + "' alt='User Image' style='width:250px ; height: 250px ; border: none ; border-radius: 10px'></p>";
 });
